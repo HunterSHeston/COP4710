d828963e049cc4c9e0d8007147882712c27e8c01
Hutner Heston <hunter_heston@knights.ucf.edu>
2017-06-27 00:41:02 -0400

    added some basic models and bootstrap

--- RSOsystem/RSOsystem/settings.py
+++ RSOsystem/RSOsystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rso.apps.RsoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- RSOsystem/rso/models.py
+++ RSOsystem/rso/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	name = models.CharField(max_length=20)
+	def __str__(self):
+		return self.name
+
+class Rso(models.Model):
+	name = models.CharField(max_length=20)
+	description = models.TextField()
+	users = models.ManyToManyField(User)
+	def __str__(self):
+		return self.name
